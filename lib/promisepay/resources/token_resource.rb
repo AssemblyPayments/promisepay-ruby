90fbfe7574df86d8d02bea56e88f4ac5ef055b19
Andrew McIntosh <andrew.mcintosh@promisepay.com>
2015-07-01 11:53:28 +1000

    - Fixed fee ids

--- lib/promisepay/resources/token_resource.rb
+++ lib/promisepay/resources/token_resource.rb
@@ -15,8 +15,8 @@ module Promisepay
     def create(type = :session, attributes)
       case type
         when :session
-          if attributes && attributes[:fees] && attributes[:fees].is_a?(Array)
-            attributes[:fees] = attributes[:fees].join(",")
+          if attributes && attributes[:fee_ids] && attributes[:fee_ids].is_a?(Array)
+            attributes[:fee_ids] = attributes[:fee_ids].join(",")
           end
           response = JSON.parse(@client.get('request_session_token', attributes).body)
           Promisepay::Token.new(@client, response.token)
