05f6257dd0f55f1167494640366db5d6bc9b1637
Romain Vigo Benia <romain.vigobenia@gmail.com>
2016-06-02 20:05:26 +0200

    Add withdraw and deposit wallet operations (not ready yet)

--- spec/promisepay/models/wallet_account_spec.rb
+++ spec/promisepay/models/wallet_account_spec.rb
@@ -1,11 +1,40 @@
 require 'spec_helper'
 
 describe Promisepay::WalletAccount do
-  let(:wallet_account) { PromisepayFactory.create_user.wallet_account }
+  let(:user) { PromisepayFactory.create_user }
+  let(:wallet_account) { user.wallet_account }
 
   describe 'user' do
     it 'returns the account owner', vcr: { cassette_name: 'wallet_accounts_user' } do
       expect(wallet_account.user).to be_a(Promisepay::User)
     end
   end
+
+  # describe 'operations' do
+  #   let(:bank_account) { PromisepayFactory.create_bank_account({}, user) }
+  #   before { user.disbursement_account(bank_account.id) }
+
+  #   describe 'withdraw', vcr: { cassette_name: 'wallet_accounts_withdraw' } do
+  #     it 'returns a disbursement' do
+  #       disbursement = wallet_account.withdraw(account_id: bank_account.id, amount: 500)
+  #       expect(disbursement).to be_a(Hash)
+  #       expect(disbursement).to have_key('id')
+  #       expect(disbursement).to have_key('amount')
+  #       expect(disbursement).to have_key('currency')
+  #       expect(disbursement['state']).to eq('pending')
+  #       expect(disbursement['to']).to eq('Bank Account')
+  #     end
+  #   end
+
+  #   describe 'deposit', vcr: { cassette_name: 'wallet_accounts_deposit' } do
+  #     it 'returns a disbursement' do
+  #       disbursement = wallet_account.deposit(account_id: bank_account.id, amount: 500)
+  #       expect(disbursement).to be_a(Hash)
+  #       expect(disbursement).to have_key('id')
+  #       expect(disbursement).to have_key('amount')
+  #       expect(disbursement).to have_key('currency')
+  #       expect(disbursement['state']).to eq('pending')
+  #     end
+  #   end
+  # end
 end
