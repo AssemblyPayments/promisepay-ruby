9ff61b00327d5fa52678df4706ba67deaebff0f3
Romain Vigo Benia <romain.vigobenia@gmail.com>
2016-06-07 22:12:36 +0200

    Add address method to company model

--- lib/promisepay/models/company.rb
+++ lib/promisepay/models/company.rb
@@ -4,12 +4,23 @@ module Promisepay
 
   	# Get the user the company belongs to.
     #
-    # @see 
+    # @see
     #
     # @return [Promisepay::User]
     def user
       response = JSON.parse(@client.get("companies/#{send(:id)}/users").body)
       Promisepay::User.new(@client, response['users'])
+    end
+
+    # Gets company address.
+    #
+    # @see https://reference.promisepay.com/#addresses
+    #
+    # @return [Hash]
+    def address
+      return nil unless @attributes.key?('related')
+      response = JSON.parse(@client.get("addresses/#{send(:related)['addresses']}").body)
+      response['addresses']
     end
   end
 end
