b8190d4c3a8a5684cf8a7398a85ba3658e547cb2
Romain Vigo Benia <romain.vigobenia@gmail.com>
2016-06-12 15:42:23 +0200

    Add deposit and withdraw methods to WalletAccount model

--- lib/promisepay/models/wallet_account.rb
+++ lib/promisepay/models/wallet_account.rb
@@ -1,33 +1,33 @@
 module Promisepay
   # Manage Wallet Accounts
   class WalletAccount < BaseModel
-    # # Withdraw funds from a Wallet Account to a specified disbursement account.
-    # #
-    # # @see https://reference.promisepay.com/#withdraw-funds
-    # #
-    # # @param options [Hash] Optional options.
-    # # @option options [String] :account_id Account to withdraw to.
-    # # @option options [Integer] :amount Amount (in cents) to withdraw.
-    # #
-    # # @return [Hash]
-    # def withdraw(options = {})
-    #   response = JSON.parse(@client.post("wallet_accounts/#{send(:id)}/withdraw", options).body)
-    #   response.key?('disbursements') ? response['disbursements'] : {}
-    # end
+    # Withdraw funds from a Wallet Account to a specified disbursement account.
+    #
+    # @see https://reference.promisepay.com/#withdraw-funds
+    #
+    # @param options [Hash] Optional options.
+    # @option options [String] :account_id Account to withdraw to.
+    # @option options [Integer] :amount Amount (in cents) to withdraw.
+    #
+    # @return [Hash]
+    def withdraw(options = {})
+      response = JSON.parse(@client.post("wallet_accounts/#{send(:id)}/withdraw", options).body)
+      response.key?('disbursements') ? response['disbursements'] : {}
+    end
 
-    # # Deposit funds to a Wallet Account from a specified payment account.
-    # #
-    # # @see https://reference.promisepay.com/#deposit-funds
-    # #
-    # # @param options [Hash] Optional options.
-    # # @option options [String] :account_id Account to deposit from.
-    # # @option options [Integer] :amount Amount (in cents) to deposit.
-    # #
-    # # @return [Hash]
-    # def deposit(options = {})
-    #   response = JSON.parse(@client.post("wallet_accounts/#{send(:id)}/deposit", options).body)
-    #   response.key?('disbursements') ? response['disbursements'] : {}
-    # end
+    # Deposit funds to a Wallet Account from a specified payment account.
+    #
+    # @see https://reference.promisepay.com/#deposit-funds
+    #
+    # @param options [Hash] Optional options.
+    # @option options [String] :account_id Account to deposit from.
+    # @option options [Integer] :amount Amount (in cents) to deposit.
+    #
+    # @return [Hash]
+    def deposit(options = {})
+      response = JSON.parse(@client.post("wallet_accounts/#{send(:id)}/deposit", options).body)
+      response.key?('disbursements') ? response['disbursements'] : {}
+    end
 
     # Show the User the Wallet Account is associated with
     #
